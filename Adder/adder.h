819251bdbbe077cea54820a8d2202d02c6c80258
Charles Dustin Smith <cdsmith@mun.ca>
2022-05-14 17:53:01 -0230

    Cmake Tutorial
    Add submodule glfw

---
+++ Adder/adder.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef _MYHEADER_H_
+#define _MYHEADER_H_
+
+float add(float a, float b);
+
+#endif 
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Adder/adder.h"
+
+int main() 
+{
+	std::cout << "Hello World from Charles Smith." << std::endl;
+	std::cout << "Testing out adder " << add( 72.3f, 73.8f ) << std::endl;
+	return 0;
+}
