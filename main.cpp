b2f6bee77d59924d749511058d5984754de318f8
Charles Dustin Smith <cdsmith@mun.ca>
2022-05-15 21:36:37 -0230

    Added the versioning for source code.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include "Adder/adder.h"
 #include <GLFW/glfw3.h>
+#include <HelloWorldConfig.h>
 
-
-int main() 
+int main( int argc, char *argv[] ) 
 {
 	std::cout << "Hello World from Charles Smith." << std::endl;
 	std::cout << "Testing out adder " << add( 72.3f, 73.8f ) << std::endl;
+	std::cout << "Program name : " << HelloWorld_NAME << std::endl;
+	std::cout << "Program Version : " << HelloWorld_VER << std::endl;
+	std::cout << "argv[0]: " << argv[0] << " Version: " << HelloWorld_VERSION_MAJOR 
+			  << "." << HelloWorld_VERSION_MINOR 
+			  << "." << HelloWorld_VERSION_PATCH << std::endl;
+	std::cout << "argc: " << argc << std::endl;
 
 	GLFWwindow *window;
 
